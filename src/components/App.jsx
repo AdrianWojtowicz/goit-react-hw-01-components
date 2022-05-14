fdc54cf2b079a187a5944eb7a925557469da504b
AdrianWojtowicz <adrian.wojtowicz@op.pl>
2022-05-14 21:51:17 +0200

    transaction

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,21 @@
 import { Profile } from "../components/Profile/Profile";
 import { Statistics } from "../components/Statistics/Statistics";
 import { FriendList } from "../components/FriendList/FriendList";
+import { TransactionHistory } from "../components/TransactionHistory/TransactionHistory";
 
 export const App = () => {
   return (
     <div
       style={{
-        height: '100vh',
         display: 'flex',
-        flexDirection: "column",
+        flexWrap: 'wrap',
         justifyContent: 'space-around',
-        alignItems: 'center',
+        alignItems: 'flex-start',
       }}>
       <Profile/>
       <Statistics />
-      <FriendList/>
+      <FriendList />
+      <TransactionHistory/>
     </div>
   );
 };
---
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -0,0 +1,25 @@
+import items from './transactions.json';
+import styles from './TransactionHistory.module.css';
+
+export const TransactionHistory = () => {
+    return (
+    <table className={styles.transaction_history}>
+        <thead className={styles.head}>
+            <tr>
+            <th className={styles.head_type} >Type</th>
+            <th className={styles.head_amount} >Amount</th>
+            <th className={styles.head_currency} >Currency</th>
+            </tr>
+        </thead>
+        <tbody>
+            {items.map(item => (
+                <tr key={item.id} className={styles.item}>
+                <td className={styles.type} >{item.type}</td>
+                <td className={styles.amount} >{item.amount}</td>
+                <td className={styles.currency}>{item.currency}</td>
+                </tr>  
+                ))}
+        </tbody>
+    </table>
+  );
+};
